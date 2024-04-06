6dadc1e27f547bcc8775ecf71fc24a506ef8328e
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-04-06 18:52:10 +0530

    Integrated Open Street Map in JavaScript

---
+++ src/js/hospitalscript.js
@@ -0,0 +1,6 @@
+var map = L.map("map-container").setView([21.1401, 79.0820], 6); 
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
