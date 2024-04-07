a8cbb473ef52ce3974b3db36b0f53c552b6a843b
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-04-07 20:19:16 +0530

    Added some Methods in JavaScript

--- src/js/hospitalscript.js
+++ src/js/hospitalscript.js
@@ -4,3 +4,15 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+function showLoader() {
+  document.querySelector(".loading").style.display = "block";
+}
+
+function hideLoader() {
+  document.querySelector(".loading").style.display = "none";
+}
+
+function findHospitals() {
+  showLoader();
+}
